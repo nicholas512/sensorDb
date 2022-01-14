ba53324ed68cc94109d0bfb30d2f4bf092da6636
Nick <nicholas512@gmail.com>
2022-01-14 21:27:18 +0000

    add SensorSelector to pick sensor from label
    also to verify no duplicates since we're moving
     this out of the SensorFileImporter method

---
+++ sensorDb-upload-observations/src/main/java/ca/carleton/gcrc/sensorDb/upload/observations/SensorSelector.java
@@ -0,0 +1,42 @@
+package ca.carleton.gcrc.sensorDb.upload.observations;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ca.carleton.gcrc.sensorDb.dbapi.Sensor;
+
+public class SensorSelector {
+    private Map<String,Sensor> sensorsByLabel;
+
+    public SensorSelector(
+        List<Sensor> sensors, 
+        List<String> labels) throws Exception{
+    
+        this.sensorsByLabel = new HashMap<String,Sensor>();
+			for(Sensor sensor : sensors){
+				
+				if( sensorsByLabel.containsKey(sensor.getLabel()) ){
+					throw new Exception("Multiple sensors with same label ("+sensor.getLabel()
+						+")"
+					);
+				}
+				
+				sensorsByLabel.put(sensor.getLabel(), sensor);
+			}
+
+    }
+
+    public Sensor getSensorFromLabel(String label){
+        Sensor sensor = null;
+		
+        for (String lab : sensorsByLabel.keySet()){
+            if (label == lab){
+                sensor = sensorsByLabel.get(lab);
+            }
+        }
+		
+		return sensor;
+    }
+}
+
